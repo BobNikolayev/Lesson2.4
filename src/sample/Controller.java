f64311efc3fa2ce49f1d7fbe3d48204263eecc1d
MrNukeBlast <bnikolayev993@gmail.com>
2020-06-28 05:29:01 -0700

    HW4.2Chat

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,17 +5,31 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 
 public class Controller {
     @FXML
     public TextField textField;
     @FXML
-    public Label label;
-    @FXML
     public TextArea textArea;
 
-    public void saveTextButton(ActionEvent actionEvent) {
-    textArea.appendText(textField.getText() + "\n");
-    textField.requestFocus();
+    public void sendTextButton(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+    }
+
+    public void textFieldSendButton(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
     }
 }
+
+// @FXML
+//    public TextArea textArea;
+//
+//    public void exitClick(ActionEvent actionEvent) {
+//        Stage stage = (Stage) textArea.getScene().getWindow();
+//        stage.close();
+//    }
+
